94c3a655590383846fadfe7b3ffb4778630e0ba2
alexy <alexy@DESKTOP-42J7OIU.wireless-1x.unc.edu>
2019-08-26 11:12:31 -0400

    A1Novice is completed.

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -7,8 +7,44 @@ public class A1Novice {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-
-		// Your code follows here.
 		
+		int custCount = scan.nextInt();
+		
+		// Declare the properties of customers.
+		String[] fName = new String[custCount];
+		String[] lName = new String[custCount];
+		double[] totCost = new double[custCount];
+		
+		for (int i = 0; i < custCount; i++) {
+			
+			// Assign values to the properties of this customer.
+			fName[i] = scan.next();
+			lName[i] = scan.next();
+			
+			int itemCount = scan.nextInt();
+			
+			// Declare the properties of items.
+			int[] itemFreq = new int[itemCount];
+			String[] itemName = new String[itemCount];
+			double[] itemPrice = new double[itemCount];
+			
+			for (int j = 0; j < itemCount; j++) {
+				
+				// Assign values to the properties of this item.
+				itemFreq[j] = scan.nextInt();
+				itemName[j] = scan.next();
+				itemPrice[j] = scan.nextDouble();
+				
+				// Add item cost to customers's total cost.
+				totCost[i] += itemFreq[j] * itemPrice[j];
+			}
+		}
+		
+		scan.close();
+		
+		// Output to the correct format.
+		for (int i = 0; i < custCount; i++) {
+			System.out.printf(fName[i].substring(0,1) + ". " + lName[i] + ": %.2f\n", totCost[i]);
+		}
 	}
 }
